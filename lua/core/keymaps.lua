1588148d3777186c7682a1487865af2f4bc518ac
VictorVinicius2004 <vvfs2004@gmail.com>
2025-08-29 14:12:49 -0300

    commit pre formatação

--- lua/core/keymaps.lua
+++ lua/core/keymaps.lua
@@ -17,6 +17,21 @@ vim.opt.shiftwidth = 2
 vim.opt.shiftround = true
 vim.opt.expandtab = true
 
-vim.keymap.set('n', '<leader>h', ':nohlsearch<CR>')
+vim.keymap.set('n', '<leader>q', ':q<CR>')
+vim.keymap.set('n', '<leader>fq', ':q!<CR>')
+vim.keymap.set('n', '<leader>wq', ':wq<CR>')
+vim.keymap.set('n', '<leader>qa', ':qa<CR>')
+vim.keymap.set('n', '<leader>wqa', ':wqa<CR>')
+vim.keymap.set('n', '<leader>n', ':nohlsearch<CR>')
 vim.keymap.set('n', '<leader>t', '<cmd>wa | vsp | terminal<cr>i')
-vim.keymap.set('n', '<leader>v', ':vsp<CR>')
+vim.keymap.set('n', '<leader>sv', ':vsp<CR>')
+vim.keymap.set('n', '<leader>ss', ':split<CR>')
+vim.keymap.set("v", "<Tab>", ">gv")
+vim.keymap.set("v", "<S-Tab>", "<gv")
+vim.keymap.set("v", "<leader>c", '"+y')
+vim.keymap.set("n", "<leader>,", '<C-w>>')
+vim.keymap.set("n", "<leader>.", '<C-w><')
+vim.keymap.set('n', '<leader>sl', '<C-w>l')
+vim.keymap.set('n', '<leader>sh', '<C-w>h')
+vim.keymap.set('n', '<leader>sj', '<C-w>j')
+vim.keymap.set('n', '<leader>sk', '<C-w>k')
